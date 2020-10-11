c59d58272084d7a03e2ef23bf797ab3e40ffb50b
Jesse Brelje <jtbrelje@gmail.com>
2020-10-11 15:26:05 -0500

    update to reflect routes, delete default page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component.ts'
+import { ResumeComponent } from './resume/resume.component.ts'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'landing', component: LandingPageComponent },
+	{ path: '', component: LandingPageComponent },
+	{ path: 'resume', component: ResumeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
